7813af184982d2c05b11a2f76ce27df20929134e
dohaKim22 <kimdoha03@gmail.com>
2025-03-14 17:47:08 +0900

    last 2025-03-14

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Music from '../components/contents/Music'
-import Movie from '../components/contents/Movie'
-import Book from '../components/contents/Book'
 
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
-import Anime from '../components/contents/Anime'
+import VideoSlider from '../components/video/VideoSlider'
+
+import { movieText } from '../data/movie'
+import { BookText } from '../data/book'
+import { AnimeText } from '../data/anime'
 
 const Home = () => {
   return (
     <Main title="유튜브 채널" description ="유튜브 채널 페이지입니다.">
           <Today/>
           <Music/>
-          <Movie/>
+          <VideoSlider videos={movieText} title='😋추천 영화를 소개합니다' id='movie'/>
+          <VideoSlider videos={BookText} title='😋추천 도서를 소개합니다' id='book'/>
+          <VideoSlider videos={AnimeText} title='😋추천 애니를 소개합니다' id='anime'/>
+          {/* <VideoSlider id='book'/>
+          <VideoSlide id='anime'r/> */}
+          {/* <Movie/>
           <Book/>
-          <Anime/>
+          <Anime/> */}
           {/* <Portfolio/>
           <Youtube/> */}
 
